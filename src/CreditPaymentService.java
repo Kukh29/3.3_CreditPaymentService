22eeedab75d2f1c359d14d43d60bcc7900861b4d
kukh29 <kukh29@gmail.com>
2022-02-13 17:22:10 +0300

    v.3

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public float calculate(int amount, int month, double percent) {
-        double mp = percent/100/12; // ежемесечная процентная ставка
-        double pow = Math.pow (1+mp,month);
-        double payment = (amount * (mp+(mp/(pow-1))));
+        double mp = percent / 100 / 12;
+        double pow = Math.pow(1 + mp, month);
+        double payment = (amount * (mp + (mp / (pow - 1))));
 
         return (float) payment;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int payment = (int) service.calculate(1_000_000, 12, 9.99d);
-        System.out.println(payment +" рублей в месяц за год");
+        System.out.println(payment + " рублей в месяц за год");
         int payment1 = (int) service.calculate(1_000_000, 24, 9.99d);
-        System.out.println(payment1 +" рублей в месяц за 2 года");
+        System.out.println(payment1 + " рублей в месяц за 2 года");
         int payment2 = (int) service.calculate(1_000_000, 36, 9.99d);
-        System.out.println(payment2 +" рублей в месяц за 3 года");
+        System.out.println(payment2 + " рублей в месяц за 3 года");
     }
 }
